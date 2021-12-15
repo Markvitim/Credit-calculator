f5e71f0b40fa3761235a18ec7baa2c6238b92c6b
Markvitim <markvitim@gmail.com>
2021-12-15 15:02:05 +0300

    formated

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        CreditPaymentService service = new CreditPaymentService ();
-        double monthlyPayment1 = service.calculate (9.99, 1_000_000, 12);
-        double monthlyPayment2 = service.calculate (9.99, 1_000_000, 24);
-        double monthlyPayment3 = service.calculate (9.99, 1_000_000, 36);
+        CreditPaymentService service = new CreditPaymentService();
+        double monthlyPayment1 = service.calculate(9.99, 1_000_000, 12);
+        double monthlyPayment2 = service.calculate(9.99, 1_000_000, 24);
+        double monthlyPayment3 = service.calculate(9.99, 1_000_000, 36);
 
         System.out.println("Ежемесячный платеж " + monthlyPayment1);
         System.out.println("Ежемесячный платеж " + monthlyPayment2);
